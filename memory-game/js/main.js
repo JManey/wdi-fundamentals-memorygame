a477e4208180c6cca580fec6b8422cf37e14a81c
JMoney-216 <jdm216@gmail.com>
2019-09-26 12:23:59 -0500

    updated game logic using objects

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,26 @@
 
-var cards =["queen", "queen", "king", "king"];
+var cards =[
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 
 var cardsInPlay = [];
 
@@ -12,10 +33,14 @@ function checkForMatch() {
 
 function flipCard(cardsId) {
 
-cardsInPlay.push(cards[cardsId]);
-console.log("User flipped " + cards[cardsId]);
+    cardsInPlay.push(cards[cardsId].rank);
+    console.log("User flipped " + cards[cardsId].rank);
+    console.log(cards[cardsId].cardImage);
+    console.log(cards[cardsId].suit);
 
-checkForMatch();
+  
+
+    checkForMatch();
 }
 
 
